148306c05fa76d05c0b79de6a20af53a386cf779
lightnightninja <mywho09@gmail.com>
2015-08-06 18:19:29 -0700

    Testing timing function, usually around 15 seconds.

--- main.c
+++ main.c
@@ -6,16 +6,59 @@
 //  Copyright (c) 2015 Cierra Shawe. All rights reserved.
 //
 
-#include <stdio.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define TEST_OPS 10000000
+
+
 
 int main() {
 
     long start = 0;
     long end;
     long sum = 0;
+    long t;
+    long temp;
+    long ops;
+    double seconds;
+
+
     printf("Please enter an integer: ");
     scanf("%li", &end);
+
+    t = clock();
+    for (long i = 1; i < TEST_OPS; i++) {
+
+        temp = TEST_OPS % i;
+        if (temp == 100) {
+            temp = 1;
+
+            printf("I'm in here for some reason\n");
+        }
+    }
+    t = clock() - t;
+
+    seconds = (double)t/(double)CLOCKS_PER_SEC;
+    seconds = 1.0 / seconds;
+    printf("Seconds = %lf\n", seconds);
+    ops = 15 * seconds * TEST_OPS;
+
+    printf("Time taken for %i operations: %lf\nIt would take %lu ops for 15 seconds of work.\n", TEST_OPS, (double)t/(double)CLOCKS_PER_SEC, ops);
+
+    t = clock();
+    for (long i = 1; i < ops; i++) {
+
+        temp = ops % i;
+        if (temp == 100) {
+            temp = 1;
+            printf("I'm in here for some reason\n");
+        }
+    }
+    t = clock() - t;
+    seconds = (double)t/(double)CLOCKS_PER_SEC;
+    printf("Time taken for %lu operations: %lf\n", ops, seconds);
 
     for (long i = start; i < end; i++) {
         for (long j = 1; j < i; j++) {
