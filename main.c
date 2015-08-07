bca813d0289192c0c4d26a903daf287dee78eedc
lightnightninja <mywho09@gmail.com>
2015-08-06 20:57:37 -0700

    Updated it for more accurate ops/sec, and calculates ranges of numbers.

--- main.c
+++ main.c
@@ -16,52 +16,58 @@
 
 int main() {
 
+    srand((unsigned int)time(NULL));
     long start = 0;
-    long end;
+    long end = 1;
     long sum = 0;
     long t;
     long temp;
     long ops;
+    long random;
+    long counter; //used for counting the ops in
     double seconds;
 
 
-    printf("Please enter an integer: ");
-    scanf("%li", &end);
-
     t = clock();
-    for (long i = 1; i < TEST_OPS; i++) {
-
-        temp = TEST_OPS % i;
-        if (temp == 100) {
-            temp = 1;
-            printf("I'm in here for some reason\n");
+    for (long i = TEST_OPS; i <= TEST_OPS; i++) {
+        for (long j = 1; j < i; j++) {
+            if (i % j == 0) { //if modding it gives you a 0, diviser
+                temp += j;
+            }
         }
+        if (temp == i) {
+            printf("%li, is a perfect number.\n", i);
+        }
+        sum = 0;
     }
     t = clock() - t;
 
     seconds = (double)t/(double)CLOCKS_PER_SEC;
     seconds = 1.0 / seconds;
-    printf("Seconds = %lf\n", seconds);
     ops = 15 * seconds * TEST_OPS;
 
     printf("Time taken for %i operations: %lf\nIt would take %lu ops for 15 seconds of work.\n", TEST_OPS, (double)t/(double)CLOCKS_PER_SEC, ops);
 
-    t = clock();
-    for (long i = 1; i < ops; i++) {
-
-        temp = ops % i;
-        if (temp == 100) {
-            temp = 1;
-            printf("I'm in here for some reason\n");
-        }
+    //random number used to reduce compiler optimization,
+    random = rand() % TEST_OPS;
+    start = random;
+    end = start;
+    temp = end;
+    //get random up to the sameish number as ops, aka find out how much work to dish
+    for (long i = random; ((i >> 1) + temp) < ops; i++, ++counter) {
+        //this bit shift is supposed to check for rounding, always less
+        temp += i;
     }
-    t = clock() - t;
-    seconds = (double)t/(double)CLOCKS_PER_SEC;
-    printf("Time taken for %lu operations: %lf\n", ops, seconds);
+    ops = temp; //setting it so we have the correct number of ops we should be doing
+    end += counter;
+
+    printf("start = %li\nend = %li\n", start, end);
+    //start timing how long it takes
+    t = clock();
 
     for (long i = start; i < end; i++) {
         for (long j = 1; j < i; j++) {
-            if (i % j == 0) {
+            if (i % j == 0) { //if modding it gives you a 0, diviser
                 sum += j;
             }
         }
@@ -70,4 +76,22 @@ int main() {
         }
         sum = 0;
     }
+
+    t = clock() - t;
+    seconds = (double)t/(double)CLOCKS_PER_SEC;
+    printf("Time taken for %lu operations: %lf\n", ops, seconds);
+    printf("Numbers range checked: %li - %li, doing %li million operations per second.\n", start, end, (long)(ops/seconds + 0.5)/1000000);
+
+
+//    for (long i = 1; i < ops; i++) {
+//
+//        temp = ops % i;
+//        if (temp == 0) {
+//            temp = 1;
+//        }
+//    }
+
+
+
+
 }
